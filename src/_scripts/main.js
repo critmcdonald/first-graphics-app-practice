3894ed0930fb4cbc41e4ccf98ff8f418ae5b9775
Christian McDonald <christian.mcdonald@utexas.edu>
2018-08-26 16:37:46 -0500

    practice updaets

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -13,7 +13,7 @@ $(() => {
 
 var chart = require('./_charts.js');
 var L = require("leaflet");
-var MiniMap = require('leaflet-minimap');
 var map = require("./_map.js");
+var MiniMap = require('leaflet-minimap');
 
 L.Icon.Default.imagePath = 'https://unpkg.com/leaflet@1.3.1/dist/images/';
